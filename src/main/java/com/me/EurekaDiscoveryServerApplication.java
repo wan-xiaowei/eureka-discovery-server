cbcbab75401add68cb308b52d7a742fd88e89bca
wanxiaowei <260483900@qq.com>
2017-08-08 14:00:54 +0800

    init

--- src/main/java/com/me/EurekaDiscoveryServerApplication.java
+++ src/main/java/com/me/EurekaDiscoveryServerApplication.java
@@ -2,10 +2,12 @@ package com.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@ServletComponentScan
 public class EurekaDiscoveryServerApplication {
 
 	public static void main(String[] args) {
